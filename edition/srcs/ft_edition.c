cbe63aa8fb45300810c8e59a14dc852cc093a616
Kevin Cabus <kcabus@z4r5p6.le-101.fr>
2018-07-17 19:08:50 +0200

    blocage si taille max

--- edition/srcs/ft_edition.c
+++ edition/srcs/ft_edition.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/06/19 08:41:27 by kcabus       #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/17 17:17:22 by kcabus      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/17 18:41:10 by kcabus      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,6 +23,7 @@ int				ft_putcharint(int c)
 
 static void		traite_signal(int s)
 {//s a traiter
+	(void)s;
 	ft_maj_win(&g_nav);
 }
 
