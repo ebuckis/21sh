f52b49191bf2822b25102437c9c6a3ee5e4881cd
shitcornichon <baptiste.pajot@gadz.org>
2018-08-22 18:12:58 +0200

    gestion redir out OK

--- exec/srcs/redir.c
+++ exec/srcs/redir.c
@@ -6,7 +6,7 @@
 /*   By: bpajot <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/23 13:32:47 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/08/22 17:04:11 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/22 18:11:15 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -46,17 +46,17 @@ static int		ft_redir_in(t_parse *p, int *i, char **env)
 	return (fd);
 }
 
-/*static int		ft_redir_close(char **redirec, int i)
+static int		ft_redir_close(t_parse *p, int *i)
 {
 	int		fd;
 
-	if (!ft_strcmp(redirec[i], ">&-") || !ft_strcmp(redirec[i], "1>&-"))
+	if (!ft_strcmp(p->arg[*i], ">&-") || !ft_strcmp(p->arg[*i], "1>&-"))
 	{
 		fd = 1;
 		dprintf(2, ">&- fd : %d\n", fd);
 		close(fd);
 	}
-	else if ((fd = ft_atoi(redirec[i])) >= 0 && ft_strstr(redirec[i], ">&-"))
+	else if ((fd = ft_atoi(p->arg[*i])) >= 0 && ft_strstr(p->arg[*i], ">&-"))
 	{
 		dprintf(2, "n>&- fd : %d\n", fd);
 		close(fd);
@@ -67,11 +67,12 @@ static int		ft_redir_in(t_parse *p, int *i, char **env)
 		return (-1);
 	}
 	return (fd);
-}*/
+}
 
 static int		ft_redir_out(t_parse *p, int *i, char **env)
 {
 	int		n;
+	int		m;
 	int		fd;
 	char	*path;
 	char	*pt;
@@ -90,7 +91,7 @@ static int		ft_redir_out(t_parse *p, int *i, char **env)
 		dprintf(2, ">> fd : %d\n", fd);
 		dup2(fd, STDOUT_FILENO);
 	}
-	else if (!ft_strcmp(p->arg[*i], ">&") || !ft_strcmp(p->arg[*i], "&>"))
+	else if (!ft_strcmp(p->arg[*i], ">&"))
 	{
 		path = get_path_redir(p, i, env);
 		fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -107,6 +108,20 @@ static int		ft_redir_out(t_parse *p, int *i, char **env)
 		dprintf(2, "n> fd : %d\n", fd);
 		dup2(fd, n);
 	}
+	else if ((n = ft_atoi(p->arg[*i])) >= 0 &&
+			(pt = ft_strstr(&(p->arg[*i][1]), ">&"))
+			&& pt[2] && (m = ft_atoi(&(pt[2]))) >=0)
+	{
+		dprintf(2, "n>&m : %d>&%d\n", n, m);
+		dup2(m, n);
+	}
+	else if ((pt = ft_strstr(p->arg[*i], ">&")) && pt[2] &&
+			(m = ft_atoi(&(pt[2]))) >=0)
+	{
+		dprintf(2, ">&m : >&%d\n", m);
+		dup2(m, STDOUT_FILENO);
+		dup2(m, STDERR_FILENO);
+	}
 	else
 	{
 		ft_putendl("redirection parse error");
@@ -115,40 +130,30 @@ static int		ft_redir_out(t_parse *p, int *i, char **env)
 	return (fd);
 }
 
-static void		ft_redir2(t_parse *p, int *redir_lim,
-		int nb_redirec, char **env)
+void			ft_redir(t_parse *p, int *redir_lim, char **env,
+		int nb_redirec)
 {
 	int		i;
 	int		fd;
 	pid_t	pid;
 
-	i = redir_lim[1] - 1;
-	fd = -1;
-	while (p->arg[++i] && p->arg_id[i][0] < PIPE)
+	if (nb_redirec)
 	{
-		if (ft_strchr(p->arg[i], '-'))
-			;//fd = ft_redir_close(p, i);
-		else if (ft_strchr(p->arg_id[i], HEREDOC))
-			;//???
-		else
+		i = redir_lim[1] - 1;
+		fd = -1;
+		while (p->arg[++i] && p->arg_id[i][0] < PIPE)
 		{
-			if (ft_strchr(p->arg[i], '>'))
-				fd = ft_redir_out(p, &i, env);
-			else if (ft_strchr(p->arg[i], '<'))
-				fd = ft_redir_in(p, &i, env);
+			if (ft_strchr(p->arg[i], '-'))
+				fd = ft_redir_close(p, &i);
+			else if (ft_strchr(p->arg_id[i], HEREDOC))
+				;//???
+			else
+			{
+				if (ft_strchr(p->arg[i], '>'))
+					fd = ft_redir_out(p, &i, env);
+				else if (ft_strchr(p->arg[i], '<'))
+					fd = ft_redir_in(p, &i, env);
+			}
 		}
 	}
-}
-
-/*void			ft_redir(char **commande, char **redirec, char **env,
-		int nb_redirec)
-{
-	if (nb_redirec)
-		ft_redir2(commande, redirec, nb_redirec, env);
-}*/
-void			ft_redir(t_parse *p, int *redir_lim, char **env,
-		int nb_redirec)
-{
-	if (nb_redirec)
-		ft_redir2(p, redir_lim, nb_redirec, env);
 }
