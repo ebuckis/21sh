d9ae0c245302f5ea491248c7662ce0b275938ce3
shitcornichon <baptiste.pajot@gadz.org>
2018-08-23 16:58:42 +0200

    yes | head toujours pas solve

--- exec/srcs/fork.c
+++ exec/srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: kcabus <kcabus@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/05/29 10:59:08 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/08/23 14:53:03 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/23 16:25:52 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -110,12 +110,13 @@ void			ft_fork_shell(t_parse *p, int *tab_pipe, char ***p_env,
 			pp[i] = (t_pid_pipe*)malloc(sizeof(t_pid_pipe));
 			dprintf(2, "pp[%d] = %p\n", i, pp[i]);
 		}
-		i = 0;
 		pp[nb_pipe + 1] = NULL;
 		pp[0]->pipeline[0] = 0;
 		pp[nb_pipe]->pid = pid;
 		pp[nb_pipe]->pipeline[1] = 1;
 		k = nb_pipe;
+		//i = 0;
+		i = nb_pipe - 1;
 		while (k-- > 0)
 		{
 			pid_pipe = ft_fork_pipe(p, tab_pipe, p_env, i);
@@ -123,7 +124,8 @@ void			ft_fork_shell(t_parse *p, int *tab_pipe, char ***p_env,
 			pp[i + 1]->pipeline[0] = pid_pipe[1];
 			pp[i]->pipeline[1] = pid_pipe[2];
 			//ft_fork_pipe(p, tab_pipe, p_env, i);
-			i++;
+			//i++;
+			i--;
 		}
 		j = -1;
 		while (pp[++j])
@@ -131,19 +133,25 @@ void			ft_fork_shell(t_parse *p, int *tab_pipe, char ***p_env,
 				j, pp[j]->pid, pp[j]->pipeline[0], pp[j]->pipeline[1]);
 		pid2 = fork();
 		if (pid2 == 0)
-			ft_execve(p, tab_pipe[i], p_env);
+			ft_execve(p, tab_pipe[nb_pipe], p_env);
 		else if (pid2 > 0)
 		{
 			waitpid(pid2, &status, WUNTRACED);
+		//	close(pp[nb_pipe]->pipeline[0]);
+			//close(pp[nb_pipe - 1]->pipeline[1]);
 			j = nb_pipe + 1;
 			while (--j > 0)
 			{
 				dprintf(2, "pp[%d]->pid = %d waiting\n", nb_pipe - j,
 					pp[nb_pipe -j]->pid);
 				dprintf(2, "close nb_pipe=%d j=%d\n", nb_pipe, j);
-				close(pp[nb_pipe - j + 1]->pipeline[0]);
-				close(pp[nb_pipe - j]->pipeline[1]);
+				dprintf(2, "pp[%d]->pipe[0]=%d\n", nb_pipe - j + 1,
+					pp[nb_pipe - j + 1]->pipeline[0]);
+				dprintf(2, "pp[%d]->pipe[1]=%d\n", nb_pipe - j,
+					pp[nb_pipe - j]->pipeline[1]);
 				waitpid(pp[nb_pipe - j]->pid, NULL, WUNTRACED);
+		//		close(pp[nb_pipe - j + 1]->pipeline[0]);
+		//		//close(pp[nb_pipe - j]->pipeline[1]);
 				dprintf(2, "pp[%d]->pid = %d finish\n", nb_pipe - j,
 					pp[nb_pipe -j]->pid);
 			}
