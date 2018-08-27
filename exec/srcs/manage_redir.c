09c7ed34612f668ca51dd0d31297c11e1d50a43e
shitcornichon <baptiste.pajot@gadz.org>
2018-08-27 16:57:39 +0200

    manage redir norme

--- exec/srcs/manage_redir.c
+++ exec/srcs/manage_redir.c
@@ -6,7 +6,7 @@
 /*   By: bpajot <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/23 13:32:33 by bpajot       #+#   ##    ##    #+#       */
-/*   Updated: 2018/08/23 11:44:45 by bpajot      ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/08/27 16:57:05 by bpajot      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -35,6 +35,30 @@ static char		**create_commande(t_parse *p, int begin, int sep)
 	return (commande);
 }
 
+static int		manage_redir2(t_parse *p, int *i, int *redir_lim,
+		int *nb_redir)
+{
+	int		begin;
+
+	begin = *i + 1;
+	while (p->arg[++(*i)] && p->arg_id[*i][0] < PIPE)
+	{
+		if (ft_strchr(p->arg_id[*i], REDIR) || ft_strchr(p->arg_id[*i],
+					HEREDOC))
+		{
+			redir_lim[1] = (redir_lim[1] != begin) ? redir_lim[1] : *i;
+			if (*i == 0)
+			{
+				ft_putendl("parse error");
+				return (1);
+			}
+			else
+				(*nb_redir)++;
+		}
+	}
+	return (0);
+}
+
 char			**manage_redir(t_parse *p, int begin, char ***p_env)
 {
 	char	**commande;
@@ -44,31 +68,15 @@ char			**manage_redir(t_parse *p, int begin, char ***p_env)
 
 	if (!(redir_lim = (int*)malloc(sizeof(int) * 2)))
 		return (NULL);
-	dprintf(2, "begin = %d\n", begin);
 	i = begin - 1;
 	redir_lim[0] = begin;
 	redir_lim[1] = begin;
 	nb_redir = 0;
 	if (p->arg[begin] && p->arg[begin][0])
 	{
-		while (p->arg[++i] && p->arg_id[i][0] < PIPE)
-		{
-			if (ft_strchr(p->arg_id[i], REDIR) || ft_strchr(p->arg_id[i],
-				HEREDOC))
-			{
-				redir_lim[1]= (redir_lim[1] != begin) ? redir_lim[1] : i;
-				if (i == 0)
-				{
-					ft_putendl("parse error");
-					return (NULL);
-				}
-				else
-					nb_redir++;
-			}
-		}
+		if (manage_redir2(p, &i, redir_lim, &nb_redir))
+			return (NULL);
 		redir_lim[1] = (redir_lim[1] != begin) ? redir_lim[1] : i;
-		dprintf(2, "nb_redir = %d\n", nb_redir);
-		dprintf(2, "begin = %d, sep = %d, size = %d\n", begin, redir_lim[1], i);
 		ft_redir(p, redir_lim, *p_env, nb_redir);
 		commande = create_commande(p, redir_lim[0], redir_lim[1]);
 	}
